20061362d85debc5b69cbe2196901eca0a616fbe
Ryan Silva <ryanaugustoo71@hotmail.com>
2023-08-22 00:11:04 -0300

    Não deu certo usar o suspense porque ele está dando conflito com outras funções assíncronas. Ver como adaptar mais tarde

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,12 @@
-import { Suspense } from 'react';
 import './header.css';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header(){
     return(
-        <Suspense fallback={<h1>Loading...</h1>}>
         <header>
             <Link className="logo" to="/">Prime Flix</Link>
             <Link className="favoritos" to="/favoritos">Meus Filmes</Link>
         </header>
-        </Suspense>
     );
 }
 
